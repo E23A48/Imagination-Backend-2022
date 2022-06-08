4937d620bbcf764c120a5115ffabfbb535489ae4
Camilo <54611785+E23A48@users.noreply.github.com>
2022-06-08 16:13:20 -0500

    Changes in oscar model
    Changes in oscar model

--- models/oscar.py
+++ models/oscar.py
@@ -1,7 +1,7 @@
 import os
 from huggingface_hub import hf_hub_download
-conceptual_weight = hf_hub_download(repo_id="akhaliq/CLIP-prefix-captioning-conceptual-weights", filename="conceptual_weights.pt")
-coco_weight = hf_hub_download(repo_id="akhaliq/CLIP-prefix-captioning-COCO-weights", filename="coco_weights.pt")
+conceptual_weight = "/home/www/imagination/models/conceptual_weights.pt"
+coco_weight = "/home/www/imagination/models/coco_weights.pt"
 import clip
 import os
 from torch import nn
